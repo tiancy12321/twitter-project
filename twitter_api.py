ee47f5413a6faa4d809f11fbc247ca4901d09bcd
tiancy12321 <chengyuan.t@wustl.edu>
2022-11-05 00:44:28 -0500

    123

--- twitter_api.py
+++ twitter_api.py
@@ -12,3 +12,12 @@ access_token = config['twitter']['access_token']
 access_token_secret = config['twitter']['access_token_secret']
 
 #authentication
+auth = tweepy.OAuthHandler(api_key, api_key_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+
+for tweet in public_tweets:
+    print(tweet.text)
