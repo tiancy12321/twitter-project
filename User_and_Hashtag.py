a0de7ba03836e7dc39c916be1a28cabe823e2ec9
tiancy12321 <chengyuan.t@wustl.edu>
2022-11-05 01:47:20 -0500

    123

---
+++ User_and_Hashtag.py
@@ -0,0 +1,39 @@
+import configparser
+import tweepy
+import pandas as pd
+
+# read configs
+config = configparser.ConfigParser()
+config.read('config.ini')
+api_key = config['twitter']['api_key']
+api_key_secret = config['twitter']['api_key_secret']
+
+access_token = config['twitter']['access_token']
+access_token_secret = config['twitter']['access_token_secret']
+
+#authentication
+auth = tweepy.OAuthHandler(api_key, api_key_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+# user tweets
+user = 'elonmusk'
+limit = 2000
+
+tweets = tweepy.Cursor(api.user_timeline,screen_name = user,
+    count = 200, tweet_mode = 'extended').items(limit)
+
+
+# tweets = api.user_timeline(screen_name = user,
+#     count = limit, tweet_mode = 'extended')
+
+# creat DataFrame
+columns = ['user', 'tweet']
+data = []
+
+for tweet in tweets:
+    data.append([tweet.user.screen_name, tweet.full_text])
+
+df = pd.DataFrame(data, columns=columns)
+df.to_csv('elonmusk_tweets.csv')
